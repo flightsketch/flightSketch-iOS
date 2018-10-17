dca18a705a0add5691f86023b99772059d9d118a
gtg738w <russ.parrish@icloud.com>
2018-10-16 18:41:47 -0700

    Initial Commit

---
+++ FlightSketchiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlightSketchiOS
+//
+//  Created by Russell P. Parrish on 10/16/18.
+//  Copyright © 2018 Russell P. Parrish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
