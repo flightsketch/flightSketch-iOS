f4193adf7b62cc41583eb3d0f1e6dbf99195cd3f
gtg738w <russ.parrish@icloud.com>
2018-10-17 08:27:14 -0700

    Adds menu bar

--- FlightSketchiOS/ViewController.swift
+++ FlightSketchiOS/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menuTrailingConst: NSLayoutConstraint!
+    
+    
+    var menuShowing = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        menuTrailingConst.constant = -200
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func toggleMenu(_ sender: Any) {
+        if menuShowing {
+            menuTrailingConst.constant = -200
+            UIView.animate(withDuration: 0.2, delay: 0.0, options: .curveEaseIn, animations: {
+                self.view.layoutIfNeeded()
+            })
+        }
+        else {
+            menuTrailingConst.constant = 0
+            UIView.animate(withDuration: 0.2, delay: 0.0, options: .curveEaseIn, animations: {
+                self.view.layoutIfNeeded()
+                })
+        }
+        
+        menuShowing = !menuShowing
+        
+    }
+    
 }
 
